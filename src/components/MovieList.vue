8a27595951fee9fc5b2dabd8b25770e269460092
Jonathan Dann <nwjon122@gmail.coml>
2017-08-14 21:40:20 -0700

    refactored to single file components

---
+++ src/components/MovieList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="movie-list">
+        <div v-for="movie in filteredMovies" class="movie">{{ movie.title }}</div>
+    </div>
+</template>
+
+<script>
+    import genres from "../util/genres";
+
+    export default {
+        data() {
+            return {
+                movies: [
+                    { title: "Cool Movie", genre: genres.ANIMATION},
+                    { title: "Home", genre: genres.DOCUMENTARY},
+                    { title: "Powers", genre: genres.FANTASY}
+                ]
+            }
+        },
+        props: ["genre", "time"],
+        methods: {
+            moviePassesGenreFilter(movie) {
+                if (!this.genre.length) {
+                    return true;
+                } else {
+                    return this.genre.find(genre => movie.genre === genre);
+                }
+            }
+        },
+        computed: {
+            filteredMovies() {
+                return this.movies.filter(this.moviePassesGenreFilter);
+            }
+        }
+    }
+</script>
