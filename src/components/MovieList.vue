11c142ad65226bfed6ea3f7496164cda38283682
Jonathan Dann <nwjon122@gmail.coml>
2017-08-19 12:18:15 -0700

    no reuslts lists filters

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -8,7 +8,7 @@
                         v-bind:time="time"></movie-item>
         </div>
         <div v-else-if="movies.length" class="no-results">
-            No Results.
+            {{ noResults }}
         </div>
         <div v-else class="no-results">
             Loading...
@@ -61,6 +61,11 @@
                 return this.movies
                     .filter(this.moviePassesGenreFilter)
                     .filter(movie => movie.sessions.find(this.sessionPassesTimeFilter));
+            },
+            noResults() {
+                let times = this.time.join(", ");
+                let genres = this.genre.join(", ");
+                return `No results for ${times}${times.length && genres.length ? ", " : ""} ${genres}`;
             }
         },
         components: {
