48f38ce245b517696a8422266f4e31ce3bb7a535
Jonathan Dann <nwjon122@gmail.coml>
2017-08-19 12:08:04 -0700

    filters working

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,7 +1,11 @@
 <template>
     <div id="movie-list">
         <div v-if="filteredMovies.length">
-            <movie-item v-for="movie in filteredMovies" v-bind:movie="movie.movie" v-bind:sessions="movie.sessions"></movie-item>
+            <movie-item v-for="movie in filteredMovies"
+                        v-bind:movie="movie.movie"
+                        v-bind:sessions="movie.sessions"
+                        v-bind:day="day"
+                        v-bind:time="time"></movie-item>
         </div>
         <div v-else-if="movies.length" class="no-results">
             No Results.
@@ -14,10 +18,11 @@
 
 <script>
     import genres from "../util/genres";
+    import times from "../util/times";
     import MovieItem from "./MovieItem.vue";
 
     export default {
-        props: ["genre", "time", "movies"],
+        props: ["genre", "time", "movies", "day"],
         methods: {
             moviePassesGenreFilter(movie) {
                 if (!this.genre.length) {
@@ -32,11 +37,30 @@
                     });
                     return matched;
                 }
+            },
+            sessionPassesTimeFilter(session) {
+
+                if (!this.day.isSame(this.$moment(session.time), "day")) {
+                    //is the session on the same day
+                    return false;
+                } else if (this.time.length === 0 || this.time.length === 2) {
+                    //if both filters are selected then show all
+                    return true;
+                } else if (this.time[0] === times.AFTER_6PM) {
+                    //if only one filter is selected, check if it is after 6
+                    //only show movies after 6
+                    return this.$moment(session.time).hour() >= 18;
+                } else {
+                    //filter is before 6, show those movies
+                    return this.$moment(session.time).hour() < 18;
+                }
             }
         },
         computed: {
             filteredMovies() {
-                return this.movies.filter(this.moviePassesGenreFilter);
+                return this.movies
+                    .filter(this.moviePassesGenreFilter)
+                    .filter(movie => movie.sessions.find(this.sessionPassesTimeFilter));
             }
         },
         components: {
