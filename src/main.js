1486590c892391afc5d4af78c2e0b80fdde4ca8c
Jonathan Dann <nwjon122@gmail.coml>
2017-08-19 20:14:52 -0700

    vue router

--- src/main.js
+++ src/main.js
@@ -1,18 +1,22 @@
 
 import Vue from "vue";
 import "./style.scss";
-import Overview from "./components/Overview.vue";
 import VueResource from "vue-resource";
 import { checkFilter } from "./util/bus"
+import VueRouter from 'vue-router';
+import routes from "./util/routes";
 
 import moment from "moment-timezone";
 moment.tz.setDefault("UTC");
 Object.defineProperty(Vue.prototype, "$moment", { get() { return this.$root.moment } });
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
 
 const bus = new Vue();
 Object.defineProperty(Vue.prototype, "$bus", {get() {return this.$root.bus}});
+
+const router = new VueRouter({ routes });
 
 new Vue({
     el: "#app",
@@ -24,13 +28,11 @@ new Vue({
         day: moment(),
         bus
     },
-    components: {
-        Overview
-    },
     created() {
         this.$http.get("/api").then(response => {
             this.movies = response.data;
         });
         this.$bus.$on('check-filter', checkFilter.bind(this));
-    }
+    },
+    router
 });
