3c4b08673d1cece965e75b61200e06b52bfb453f
Jonathan Dann <nwjon122@gmail.coml>
2017-08-19 20:40:17 -0700

    movie detail page

--- src/util/routes.js
+++ src/util/routes.js
@@ -2,6 +2,7 @@ import Overview from "../components/Overview.vue";
 import Detail from "../components/Detail.vue";
 
 export default [
-    { path: "/", component: Overview },
-    { path: "/movie", component: Detail }
+    { path: "/", component: Overview, name: "home" },
+    { path: "/movie/:id", component: Detail, name: "movie" },
+    { path: "*", redirect: { name: "home"} }
 ];
