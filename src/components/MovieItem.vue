e308106c749cf4d09becfaba6d47e94e28bd21fa
Jonathan Dann <nwjon122@gmail.coml>
2017-08-19 21:00:13 -0700

    detail page complete

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="movie">
         <div class="movie-col-left">
-            <img v-bind:src="movie.Poster"/>
+
+            <router-link v-bind:to="{ name: 'movie', params: { id: movie.imdbID } }">
+                <img v-bind:src="movie.Poster"/>
+            </router-link>
+
         </div>
         <div class="movie-col-right">
             <div class="movie-title">
@@ -12,45 +16,15 @@
 
                 <span class="movie-rating">{{ movie.Rated }}</span>
             </div>
-            <div class="movie-sessions">
-                <div v-for="session in filteredSessions(sessions)" class="session-time-wrapper">
-                    <div class="session-time">{{ formatSessionTime(session.time) }}</div>
-                </div>
-            </div>
+            <slot></slot>
         </div>
     </div>
 </template>
 
 <script>
-    import times from "../util/times";
 
     export default {
-        props: ["movie", "sessions", "day", "time"],
-        methods: {
-            formatSessionTime(raw) {
-                return this.$moment(raw).format("h:mm A");
-            },
-            filteredSessions(sessions) {
-                return sessions.filter(this.sessionPassesTimeFilter);
-            },
-            sessionPassesTimeFilter(session) {
-
-                if (!this.day.isSame(this.$moment(session.time), "day")) {
-                    //is the session on the same day
-                    return false;
-                } else if (this.time.length === 0 || this.time.length === 2) {
-                    //if both filters are selected then show all
-                    return true;
-                } else if (this.time[0] === times.AFTER_6PM) {
-                    //if only one filter is selected, check if it is after 6
-                    //only show movies after 6
-                    return this.$moment(session.time).hour() >= 18;
-                } else {
-                    //filter is before 6, show those movies
-                    return this.$moment(session.time).hour() < 18;
-                }
-            }
-        }
+        props: ["movie"]
     }
 
 </script>
